3653b6b80d641811beb2cb15f6638f791a2de6a9
Ihor <mr.laptev.goga@mail.ru>
2022-04-03 15:21:49 +0300

    first successful run

--- LectionCatalog/StartUp.cs
+++ LectionCatalog/StartUp.cs
@@ -2,5 +2,38 @@
 {
     public class StartUp
     {
+        public IConfiguration Configuration { get; }
+        public StartUp(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            //app.UseSession();
+
+            //app.UseAuthentication();
+            //app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
     }
 }
